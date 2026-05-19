5740d5d6ebaa53af1773fec5ea12858e9de3569e
Sebastian Arias <juansearias21@gmail.com>
2026-05-19 16:01:27 -0500

    feat(SCRUM-17): validar disponibilidad de horarios en citas

--- bh-core/src/app.module.ts
+++ bh-core/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AppointmentsModule } from './appointments/appointments.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AppointmentsModule,
+  ],
   controllers: [],
   providers: [],
 })
